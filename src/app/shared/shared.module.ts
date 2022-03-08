4d4e6a89f986ee8b6156400fda6da4f9567b97c7
DiegoCeccon <dceccon.dextra@alelo.com.br>
2022-03-07 23:59:04 -0300

    refactoring architetural

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { TableModule } from "./components/table-custom/table.module";
+import { TableModule } from "./components/table/table.module";
 
 @NgModule({
   declarations: [],
