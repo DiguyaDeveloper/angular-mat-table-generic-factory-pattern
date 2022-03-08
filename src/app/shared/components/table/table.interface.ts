fb8246d880cd9733b3d51f47353acd3de3324d3e
DiegoCeccon <dceccon.dextra@alelo.com.br>
2022-03-08 00:02:28 -0300

    remove invalid

--- src/app/shared/components/table/table.interface.ts
+++ src/app/shared/components/table/table.interface.ts
@@ -1,18 +1,12 @@
 import { MatTableDataSource } from "@angular/material/table";
 import { BehaviorSubject } from "rxjs";
 import { TableColumns } from "src/app/core/interfaces/table-columns.interface";
+import { PaginatorType } from "src/app/core/types/paginator.type";
 
-type PaginatorType = "FRONT" | "BACK";
-
-enum PaginatorEnum {
-  FRONT = "FRONT",
-  BACK = "BACK",
-}
 export interface TableInterface<T> {
   columns: BehaviorSubject<TableColumns<T>[]>;
   dataSource: MatTableDataSource<T>;
   paginationType: PaginatorType;
-  expandable: PaginatorType;
 
   setDataSource(value: T[]): void;
   getColumnsToDisplay(): string[];
