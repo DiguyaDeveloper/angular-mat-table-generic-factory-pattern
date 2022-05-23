1a36d573a228a8c732145c1ab704db8f627ba6ec
DiegoCeccon <dceccon.dextra@alelo.com.br>
2022-05-23 15:25:18 -0300

    update package

--- src/app/shared/components/table/table.module.ts
+++ src/app/shared/components/table/table.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from "@angular/common";
 import { MatTableModule } from "@angular/material/table";
 import { MatSortModule } from "@angular/material/sort";
 import { TableComponent } from "./table.component";
-import { HeaderModule } from "../header/header.module";
+import { HeaderModule } from "./header/header.module";
 import { PaginatorModule } from "../paginator/paginator.module";
-import { CellModule } from "../cell/cell.module";
 import { MatSelectModule } from "@angular/material/select";
 import { MatCheckboxModule } from "@angular/material/checkbox";
 import { MatIconModule } from "@angular/material/icon";
+import { CellModule } from "./cell/cell.module";
 @NgModule({
   declarations: [TableComponent],
   imports: [
