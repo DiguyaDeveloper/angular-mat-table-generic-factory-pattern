ca684af437e4cdcbe768e1d6658671f8b6f982a9
DiegoCeccon <dceccon.dextra@alelo.com.br>
2022-06-01 15:51:56 -0300

    quebrado em 2 exemplos de table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  OnInit,
-  TemplateRef,
-  ViewChild,
-} from "@angular/core";
-import { TableColumns } from "./core/interfaces/table-columns.interface";
-import { TablePaginationAbstract } from "./core/models/table.abstract";
-import { InventoryProduct, TableService } from "./core/services/list.service";
-import { Table } from "./shared/components/table/table.class";
-
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
